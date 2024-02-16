95db028efc50de9cd82435098cbe9e7a7f2b65bf
James Ormrod <88646794+JamesOrmrod@users.noreply.github.com>
2024-02-16 20:26:01 +1100

    Added functioning rock-paper-scissors buttons

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,55 @@
+
+const choiceBtns = document.querySelectorAll("button");
+
+let roundNum = 0;
+let playerScore = 0;
+let compScore = 0;
+
+
+choiceBtns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        playerChoice = e.target.id;
+        roundNum++;
+        let roundResult = getRoundResult(playerChoice,
+                                    getComputerChoice());
+    
+        const outputDiv = document.querySelector(".outputContainer");
+        const para = document.createElement("p")
+    
+        para.textContent = 'Round ' + roundNum + ': ' + roundResult;
+    
+        outputDiv.appendChild(para);
+        
+        console.log('Round ' + roundNum + ': ' + roundResult);
+
+        const scores = document.querySelector("#scores");
+        const rounds = document.querySelector("#rounds");
+
+        scores.textContent = `Player Score: ${playerScore} | Computer Score: ${compScore}`;
+        rounds.textContent = `Rounds played: ${roundNum}`;
+
+        if (roundNum === 5) {
+            const scoreDiv = document.querySelector(".scoreContainer");
+            const gameOverPara = document.createElement("p");
+
+            if (playerScore === compScore) {
+                gameOverPara.textContent = "It's a tie! Game over."
+            } else if (playerScore > compScore) {
+                gameOverPara.textContent = "Congratulations, you won! Game over."
+            } else {
+                gameOverPara.textContent = "Sorry, you lost. Game over."
+            }
+            scoreDiv.appendChild(gameOverPara);
+
+            roundNum = 0;
+            playerScore = 0;
+            compScore = 0;
+        }
+    
+    });
+});
+
+
 // function to get the computer's choice in R-P-S
 function getComputerChoice() {
     // get a random number 0, 1 or 2
@@ -10,7 +62,7 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function getRoundResult(playerSelection, computerSelection) {
     // trim the player selection and make only the first letter capital
     playerSelection = makeCapitalCase(playerSelection.trim());
 
@@ -21,28 +73,37 @@ function playRound(playerSelection, computerSelection) {
                 case "Rock":
                     return getResultMessage("tie", playerSelection, computerSelection);
                 case "Paper":
+                    compScore++;
                     return getResultMessage("lose", playerSelection, computerSelection);
                 case "Scissors":
+                    playerScore++;
                     return getResultMessage("win", playerSelection, computerSelection);
             }
+            break ;
         case "Paper":
             switch (computerSelection) {
                 case "Rock":
+                    playerScore++;
                     return getResultMessage("win", playerSelection, computerSelection);
                 case "Paper":
                     return getResultMessage("tie", playerSelection, computerSelection);
                 case "Scissors":
+                    compScore++;
                     return getResultMessage("lose", playerSelection, computerSelection);
             }
+            break ;
         case "Scissors":
             switch (computerSelection) {
                 case "Rock":
+                    compScore++;
                     return getResultMessage("lose", playerSelection, computerSelection);
                 case "Paper":
+                    playerScore++;
                     return getResultMessage("win", playerSelection, computerSelection);
                 case "Scissors":
                     return getResultMessage("tie", playerSelection, computerSelection);
             }
+            break ;
         default:
             return "You did not enter a valid choice."
     }
@@ -60,19 +121,5 @@ function getResultMessage(result, playerSelection, computerSelection) {
             return `You lose! ${computerSelection} beats ${playerSelection}.`;
         case "tie":
             return `Tie! You both selected ${playerSelection}.`;
-    }
-}
-
-
-// let computerSelection = getComputerChoice();
-// let playerSelection = "rock";
-
-// console.log(playRound(playerSelection, computerSelection));
-
-function playGame() {
-    for (let roundNum = 1; roundNum <= 5; roundNum++) {
-        // let playerInput = prompt();
-        let roundResult = playRound(prompt("Please enter your choice: Rock, Paper or Scissors"), getComputerChoice());
-        console.log('Round ' + roundNum + ': ' + roundResult);
     }
 }
